b4b5da261944f5433616b6e135e9c4449db3a6aa
Ubuntu <ubuntu@ip-172-31-45-231.us-west-2.compute.internal>
2018-05-10 17:41:39 +0000

    Added tags

--- main.tf
+++ main.tf
@@ -67,6 +67,14 @@ resource "aws_autoscaling_group" "nginx_autoscaling_group" {
   }
 
   depends_on = ["aws_launch_configuration.nginx_server"]
+
+  tags = [
+    {
+      key                 = "Name"
+      value               = "Nginx"
+      propagate_at_launch = "true"
+    },
+  ]
 }
 
 
